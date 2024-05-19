7293d933b4ca4f581682ee21c46d95aab2d0b8c5
JarolyP <jobs13az@gmail.com>
2024-05-18 22:20:51 -0400

    agrengando un evento para abrir la galeria

---
+++ src/eventoCategorias.js
@@ -0,0 +1,13 @@
+const contenedorCategorias = document.getElementById('categorias')
+const galeria = document.getElementById('galeria')
+
+
+contenedorCategorias.addEventListener('click', (e) => {
+    e.preventDefault()
+    
+    console.log(e.target.closest('a'))
+    if(e.target.closest('a')){
+        galeria.classList.add('galeria--active')
+        document.body.style.overflow = 'hidden'
+    }
+})
