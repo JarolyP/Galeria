2464d37a8d9cf580b12f7a4b622f44b13b86d0b6
JarolyP <jobs13az@gmail.com>
2024-05-28 17:01:18 -0400

    haciendo la parte 2 de el carousel

--- src/datos/galeria/carousel.js
+++ src/datos/galeria/carousel.js
@@ -1,5 +1,24 @@
+const galeria = document.getElementById('galeria')
 const carousel = (direccion) => {
-    console.log(direccion)
+    const opciones = {
+        root: document.querySelector('.galeria__carousel'),
+        rootMagin: '0px',
+        treshold: 0.9
+    }
+
+    const observer = new IntersectionObserver((entradas) => {
+        console.log(entradas[0]);
+
+        const slides = galeria.querySelectorAll('.galeria__carousel-slide')
+        slides.forEach(slide => {
+            observer.unobserve(slide)
+        });
+    }, opciones);
+
+    const slides = galeria.querySelectorAll('.galeria__carousel-slide')
+    slides.forEach(slide => {
+        observer.observe(slide)
+    });
 }
 
 export default carousel;
